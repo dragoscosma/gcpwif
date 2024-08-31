6e4f1430ab541b340be009028efc3491b94dcc33
dragoscosma <dragos_cosma@live.com>
2024-08-30 20:25:30 -0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,7 @@
+resource "google_storage_bucket" "my-bucket-wirftest" {
+  name          = "bkt-demo-000"
+  location      = "us-central1"
+  project = "test-dcosec"
+  force_destroy = true
+  public_access_prevention = "enforced"
+}
